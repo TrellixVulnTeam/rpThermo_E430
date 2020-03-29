b341f07805f7d05f68e7be8348f0e6a603e677aa
mdulac <melchior.dulac@inra.fr>
2020-03-29 14:06:04 +0200

    added num workers option

--- rpToolServe.py
+++ rpToolServe.py
@@ -18,146 +18,7 @@ import rpToolCache
 import rpSBML
 
 
-###################################################################################
-################################## processify #####################################
-###################################################################################
-
-#hack to stop the memory leak. Indeed it seems that looping through rpFBA and the rest causes a memory leak... According to: https://github.com/opencobra/cobrapy/issues/568 there is still memory leak issues with cobrapy. looping through hundreds of models and running FBA may be the culprit
-
-import inspect
-import traceback
-import signal
-from functools import wraps
-from multiprocessing import Process, Queue
-
-'''
-This is to deal with an error caused by segmentation fault
-'''
-def handler(signum, frame):
-    raise OSError('Code is throwing a segmentation fault')
-
-
-class Sentinel:
-    pass
-
-
-def processify(func):
-    '''Decorator to run a function as a process.
-    Be sure that every argument and the return value
-    is *pickable*.
-    The created process is joined, so the code does not
-    run in parallel.
-    '''
-
-    def process_generator_func(q, *args, **kwargs):
-        result = None
-        error = None
-        it = iter(func())
-        while error is None and result != Sentinel:
-            try:
-                result = next(it)
-                error = None
-            except StopIteration:
-                result = Sentinel
-                error = None
-            except Exception:
-                ex_type, ex_value, tb = sys.exc_info()
-                error = ex_type, ex_value, ''.join(traceback.format_tb(tb))
-                result = None
-            q.put((result, error))
-
-    def process_func(q, *args, **kwargs):
-        try:
-            result = func(*args, **kwargs)
-        except Exception:
-            ex_type, ex_value, tb = sys.exc_info()
-            error = ex_type, ex_value, ''.join(traceback.format_tb(tb))
-            result = None
-        else:
-            error = None
-
-        q.put((result, error))
-
-    def wrap_func(*args, **kwargs):
-        # register original function with different name
-        # in sys.modules so it is pickable
-        process_func.__name__ = func.__name__ + 'processify_func'
-        setattr(sys.modules[__name__], process_func.__name__, process_func)
-
-        signal.signal(signal.SIGCHLD, handler) #This is to catch the segmentation error
-
-        q = Queue()
-        p = Process(target=process_func, args=[q] + list(args), kwargs=kwargs)
-        p.start()
-        result, error = q.get()
-        p.join()
-
-        if error:
-            ex_type, ex_value, tb_str = error
-            message = '%s (in subprocess)\n%s' % (str(ex_value), tb_str)
-            raise ex_type(message)
-
-        return result
-
-    def wrap_generator_func(*args, **kwargs):
-        # register original function with different name
-        # in sys.modules so it is pickable
-        process_generator_func.__name__ = func.__name__ + 'processify_generator_func'
-        setattr(sys.modules[__name__], process_generator_func.__name__, process_generator_func)
-
-        signal.signal(signal.SIGCHLD, handler) #This is to catch the segmentation error
-
-        q = Queue()
-        p = Process(target=process_generator_func, args=[q] + list(args), kwargs=kwargs)
-        p.start()
-
-        result = None
-        error = None
-        while error is None:
-            result, error = q.get()
-            if result == Sentinel:
-                break
-            yield result
-        p.join()
-
-        if error:
-            ex_type, ex_value, tb_str = error
-            message = '%s (in subprocess)\n%s' % (str(ex_value), tb_str)
-            raise ex_type(message)
-
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        if inspect.isgeneratorfunction(func):
-            return wrap_generator_func(*args, **kwargs)
-        else:
-            return wrap_func(*args, **kwargs)
-    return wrapper
-
-
-###########################################################
-################## multiprocesses run #####################
-###########################################################
-
-#This is a non-deamonic multiprocessing method that can be used in combination with processify
-
-import multiprocessing
-import multiprocessing.pool
-
-
-class NoDaemonProcess(multiprocessing.Process):
-    # make 'daemon' attribute always return False
-    def _get_daemon(self):
-        return False
-    def _set_daemon(self, value):
-        pass
-    daemon = property(_get_daemon, _set_daemon)
-
-# We sub-class multiprocessing.pool.Pool instead of multiprocessing.Pool
-# because the latter is only a wrapper function, not a proper class.
-class nonDeamonicPool(multiprocessing.pool.Pool):
-    Process = NoDaemonProcess
-
-
+import concurrent.futures
 
 ##
 #
@@ -188,66 +49,55 @@ def runThermo_mem(rpthermo, inputTar, outTar, pathway_id):
                     tf.addfile(tarinfo=info, fileobj=fiOut)
 
 
-##
-#
-#
-def runThermo_hdd(rpthermo, inputTar, outputTar, pathway_id='rp_pathway'):
-    with tempfile.TemporaryDirectory() as tmpOutputFolder:
-        with tempfile.TemporaryDirectory() as tmpInputFolder:
-            tar = tarfile.open(fileobj=inputTar, mode='r:xz')
-            tar.extractall(path=tmpInputFolder)
-            tar.close()
-            for sbml_path in glob.glob(tmpInputFolder+'/*'):
-                file_name = sbml_path.split('/')[-1].replace('.sbml', '').replace('.xml', '').replace('.rpsbml', '')
-                rpsbml = rpSBML.rpSBML(file_name)
-                rpsbml.readSBML(sbml_path)
-                rpthermo.pathway_drG_prime_m(rpsbml, pathway_id)
-                rpsbml.writeSBML(tmpOutputFolder)
-                rpsbml = None
-            with tarfile.open(fileobj=outputTar, mode='w:xz') as ot:
-                for sbml_path in glob.glob(tmpOutputFolder+'/*'):
-                    file_name = str(sbml_path.split('/')[-1].replace('.sbml', '').replace('.xml', '').replace('.rpsbml', ''))
-                    file_name += '.rpsbml.xml'
-                    info = tarfile.TarInfo(file_name)
-                    info.size = os.path.getsize(sbml_path)
-                    ot.addfile(tarinfo=info, fileobj=open(sbml_path, 'rb'))
+
+def chunkIt(seq, num):
+    avg = len(seq) / float(num)
+    out = []
+    last = 0.0
+    while last < len(seq):
+        out.append(seq[int(last):int(last + avg)])
+        last += avg
+    return out
 
 
 ## Less memory effecient than the _hdd method but faster
 #
 #
-@processify
-def singleThermo_multi(file_name, sbml_path, pathway_id, tmpOutputFolder):
+def singleThermo(sbml_paths, pathway_id, tmpOutputFolder):
     rpcache = rpToolCache.rpToolCache()
     rpthermo = rpThermo.rpThermo()
     rpthermo.kegg_dG = rpcache.kegg_dG
     rpthermo.cc_preprocess = rpcache.cc_preprocess
-    rpsbml = rpSBML.rpSBML(file_name)
-    rpsbml.readSBML(sbml_path)
-    rpthermo.pathway_drG_prime_m(rpsbml, pathway_id)
-    rpsbml.writeSBML(tmpOutputFolder)
-    rpsbml = None
+    for sbml_path in sbml_paths:
+        file_name = sbml_path.split('/')[-1].replace('.sbml', '').replace('.xml', '').replace('.rpsbml', '')
+        rpsbml = rpSBML.rpSBML(file_name)
+        rpsbml.readSBML(sbml_path)
+        rpthermo.pathway_drG_prime_m(rpsbml, pathway_id)
+        rpsbml.writeSBML(tmpOutputFolder)
+        rpsbml = None
+
 
 ## Multiprocessing implementation of the thermodynamics package
 #
 #
-def runThermo_multi(inputTar, outputTar, pathway_id='rp_pathway'):
+def runThermo_multi(inputTar, outputTar, num_workers=10, pathway_id='rp_pathway'):
     with tempfile.TemporaryDirectory() as tmpOutputFolder:
         with tempfile.TemporaryDirectory() as tmpInputFolder:
             tar = tarfile.open(fileobj=inputTar, mode='r:xz')
             tar.extractall(path=tmpInputFolder)
             tar.close()
-            #HERE SPECIFY THE NUMBER OF CORES
-            #pool = multiprocessing.Pool(processes=10)
-            pool = nonDeamonicPool(processes=10)
-            results = []
-            for sbml_path in glob.glob(tmpInputFolder+'/*'):
-                file_name = sbml_path.split('/')[-1].replace('.sbml', '').replace('.xml', '').replace('.rpsbml', '')
-                results.append(pool.apply_async(rpthermo.singleThermo_multi, args=(file_name, sbml_path, pathway_id, tmpOutputFolder,)))
-            output = [p.get() for p in results]
-            logging.info(output)
-            pool.close()
-            pool.join()
+            with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
+                jobs = {}
+                #split the files "equally" between all workers
+                sep_li = chunkIt(glob.glob(tmpInputFolder+'/*'), num_workers)
+                for s_l in sep_li:
+                    jobs[executor.submit(singleThermo, s_l, pathway_id, tmpOutputFolder)] = s_l
+                for future in concurrent.futures.as_completed(jobs):
+                    f_n = jobs[future]
+                    try:
+                        data = future.result()
+                    except Exception as exc:
+                        print('%r generated an exception: %s' % (f_n, exc))
             with tarfile.open(fileobj=outputTar, mode='w:xz') as ot:
                 for sbml_path in glob.glob(tmpOutputFolder+'/*'):
                     file_name = str(sbml_path.split('/')[-1].replace('.sbml', '').replace('.xml', '').replace('.rpsbml', ''))
@@ -257,14 +107,14 @@ def runThermo_multi(inputTar, outputTar, pathway_id='rp_pathway'):
                     ot.addfile(tarinfo=info, fileobj=open(sbml_path, 'rb'))
 
 
+
 ##
 #
 #
-def main(inputTar, outputTar, pathway_id='rp_pathway'):
+def main(inputTar, outputTar, num_workers=10, pathway_id='rp_pathway'):
     with open(inputTar, 'rb') as inputTar_bytes:
         outputTar_bytes = io.BytesIO()
-        #runThermo_hdd(rpthermo, inputTar_bytes, outputTar_bytes, pathway_id)
-        runThermo_multi(rpthermo, inputTar_bytes, outputTar_bytes, pathway_id)
+        runThermo_multi(inputTar_bytes, outputTar_bytes, num_workers, pathway_id)
         ########## IMPORTANT #####
         outputTar_bytes.seek(0)
         ##########################
